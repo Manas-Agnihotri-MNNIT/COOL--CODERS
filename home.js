83c0df1e665e93278dfeef026105d8ea6a85a4de
gaurav9479 <gauravshivmurat2@gmail.com>
2025-02-08 05:48:12 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,19 +1,49 @@
 const ctx = document.getElementById('myChart');
-
+const ctm = document.getElementById('myhart');
+//weekly
 new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Food', 'Social Life', 'Pets', 'Transport', 'Household', 'Education','Others'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [30,50,60,35,52,27,29],
             borderWidth: 1
         }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (tooltipItem) {
+                        let value = tooltipItem.raw;
+                        return `₹ ${value.toFixed(2)}`;
+                    }
+                }   
+            }
+        }
+    }
+});
+//monthly
+new Chart(ctm, {
+    type: 'pie',
+    data: {
+        labels: ['Food', 'Social Life', 'Pets', 'Transport', 'Household', 'Education','Others'],
+        datasets: [{
+            label: '# of Votes',
+            data: [3,5,6,8,5,2,6],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (tooltipItem) {
+                        let value = tooltipItem.raw;
+                        return `₹ ${value.toFixed(2)}`;
+                    }
+                }   
             }
         }
     }
